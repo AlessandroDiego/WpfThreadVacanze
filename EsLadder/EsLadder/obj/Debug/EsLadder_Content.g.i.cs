04b80f5f33c5a64ca935dced1c44a1a1d9f5879e
AlessandroDiego <alessandro.diego.stud@ispascalcomandini.it>
2020-12-19 15:53:59 +0100

    Inserimento Foto:Cintura Editata e Logo

--- EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
+++ EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sfondoring.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cintura.png")]
 
 
