36abb35c69f02e8a8d535880553541856e09862f
AlessandroDiego <alessandro.diego.stud@ispascalcomandini.it>
2020-12-19 16:09:11 +0100

    Inserimento Foto: Scala

--- EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
+++ EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("cintura.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("scala.png")]
 
 
