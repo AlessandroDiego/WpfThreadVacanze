6bae53d35e57d16792bbf3621c52eb76dd6e4e6c
AlessandroDiego <alessandro.diego.stud@ispascalcomandini.it>
2020-12-26 14:01:13 +0100

    Aggiunta Foto : Wrestlers

--- EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
+++ EsLadder/EsLadder/obj/Debug/EsLadder_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("scala.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wrestler%202.png")]
 
 
